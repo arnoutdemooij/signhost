0df4880e3d880c107548ee2f257109178d0f1974
Arnout de Mooij <arnoutdemooij@gmail.com>
2016-11-22 19:58:31 +0100

    new API routes, suitable for multi document transactions

--- lib/sign_host/api_client.rb
+++ lib/sign_host/api_client.rb
@@ -19,21 +19,12 @@ module SignHost
       }
     end
 
-    def put_file(file_id, file)
-      RestClient.put(upload_file_url(file_id), file, auth_headers.merge(multipart: true, content_type: 'application/pdf')) { |response, request, result, &block |
+    def add_file(transaction_id, file_id, file, sh_display_name=nil)
+      headers = auth_headers.merge(multipart: true, content_type: 'application/pdf')
+      headers.merge!('SH-DisplayName' => sh_display_name) if sh_display_name.present?
+      RestClient.put(add_file_url(transaction_id, file_id), file, headers) { |response, request, result, &block |
         case response.code
-        when 200
-          true
-        else
-          response.return!(request, result, &block)
-        end
-      }
-    end
-
-    def add_file(transaction_id, file_id, file)
-      RestClient.put(add_file_url(transaction_id, file_id), file, auth_headers.merge(multipart: true, content_type: 'application/pdf')) { |response, request, result, &block |
-        case response.code
-        when 200
+        when 200, 201, 204
           true
         else
           response.return!(request, result, &block)
@@ -44,7 +35,7 @@ module SignHost
     def start_transaction(transaction_id)
       RestClient.put(start_transaction_url(transaction_id), nil, auth_headers.merge(content_type: 'application/json', accept: 'application/json')) { |response, request, result, &block|
         case response.code
-        when 200
+        when 200, 201, 204
           JSON.parse(response.body)
         else
           response.return!(request, result, &block)
@@ -74,26 +65,26 @@ module SignHost
       end
     end
 
-    def get_signed_document(file_id, file_options={})
-      RestClient.get(signed_document_url(file_id), auth_headers) { |response, request, result, &block |
+    def get_signed_document(transaction_id, file_id, file_options={})
+      RestClient.get(signed_document_url(transaction_id, file_id), auth_headers) { |response, request, result, &block |
         case response.code
-        when 200
+        when 200, 201, 204
           file = Tempfile.new(['signed-document', '.pdf'], file_options)
           file.write response.body
-          file.path
+          file
         else
           response.return!(request, result, &block)
         end
       }
     end
 
-    def get_receipt(transaction_id)
+    def get_receipt(transaction_id, file_options={})
       RestClient.get(receipt_url(transaction_id), auth_headers) { |response, request, result, &block |
         case response.code
-        when 200
-          file = Tempfile.new(['receipt', '.pdf'])
+        when 200, 201, 204
+          file = Tempfile.new(['receipt', '.pdf'], file_options)
           file.write response.body
-          file.path
+          file
         else
           response.return!(request, result, &block)
         end
@@ -103,31 +94,27 @@ module SignHost
     private
 
     def new_transaction_url
-      api_url + "transaction"
-    end
-
-    def upload_file_url(file_id)
-      api_url + "file/" + file_id
+      "#{api_url}transaction"
     end
 
     def add_file_url(transaction_id, file_id)
-      api_url + "transaction/" + transaction_id + "/file/" + file_id
+      "#{api_url}transaction/#{transaction_id}/file/#{file_id}"
     end
 
     def start_transaction_url(transaction_id)
-      api_url + "transaction/" + transaction_id + "/start"
+      "#{api_url}transaction/#{transaction_id}/start"
     end
 
     def transaction_url(transaction_id)
-      api_url + "transaction/" + transaction_id
+      "#{api_url}transaction/#{transaction_id}"
     end
 
-    def signed_document_url(file_id)
-      api_url + "file/document/#{file_id}"
+    def signed_document_url(transaction_id, file_id)
+      "#{api_url}transaction/#{transaction_id}/file/#{file_id}"
     end
 
     def receipt_url(transaction_id)
-      api_url + "file/receipt/#{transaction_id}"
+      "#{api_url}file/receipt/#{transaction_id}"
     end
 
     def api_url
