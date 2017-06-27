3f9d695072407ccab16fc2821c390d584c6e2f4c
Arnout de Mooij <arnoutdemooij@gmail.com>
2017-06-27 17:15:16 +0200

    change function name

--- lib/sign_host/api_client.rb
+++ lib/sign_host/api_client.rb
@@ -43,7 +43,7 @@ module SignHost
       }
     end
 
-    def transaction_upload_meta_data(transaction_id, file_id, meta_data)
+    def transaction_add_file_meta_data(transaction_id, file_id, meta_data)
       RestClient.put(transaction_upload_file_url(transaction_id, file_id), meta_data.to_json, auth_headers.merge(content_type: 'application/json', accept: 'application/json')) { |response, request, result, &block |
         case response.code
         when 200, 201, 204
